8da9d1c1481990cdfba5ef96e55992b20e2b7c98
rindra <rindra.randriambololona@gmail.com>
2018-09-02 18:36:23 +0200

    nice menu... but not perfect!

--- src/App.js
+++ src/App.js
@@ -1,15 +1,52 @@
 import React, { Component } from 'react';
+import Header from './Header';
+import Menu from './menu';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './App.css';
-import Header from './Header'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showMenu: false,
+    };
+
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+
+  toggleMenu() {
+    this.setState(prevState => ({
+      showMenu: !prevState.showMenu
+    }));
+  }
+
   render() {
     return (
       <div className="App">
-        <Header />
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Menu showMenu={this.state.showMenu} />
+
+        <div id="content">
+          <nav className="bt-menu">
+            <div>
+
+              <button type="button" id="sidebarCollapse" className="btn btn-info" onClick={this.toggleMenu}>
+                <FontAwesomeIcon icon={this.state.showMenu ? "times" : "bars"} size='lg' />
+              </button>
+
+            </div>
+          </nav>
+
+          <Header />
+
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+
+        <div className={this.state.showMenu ? "overlay active" : "overlay hide"} onClick={this.toggleMenu}></div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faStroopwafel, faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
+import './bootstrap.min.css';
 import './index.css';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+library.add(faStroopwafel, faBars, faTimes);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
