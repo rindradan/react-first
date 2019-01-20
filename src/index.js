2b37fdfcb2c2f329843ec2a2c8527d50ccec2252
rindradan <rindra.randriambololona@gmail.com>
2019-01-20 20:42:05 +0100

    bootstrap v4.1.0

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faStroopwafel, faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
-import './assets/bootstrap/bootstrap.min.css';
+import './assets/bootstrap/bootstrap.min.css'; // Bootstrap v4.1.0
 
 import './assets/less/index.css';
 
